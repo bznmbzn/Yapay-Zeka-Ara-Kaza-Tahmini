391af449fbc14345d3ca596b4f338a6091e0d07a
mehmet bozan <mehmetbozan44@gmail.com>
2016-06-12 11:36:39 +0300

    Update kaza_tahmini.m

--- kaza_tahmini.m
+++ kaza_tahmini.m
@@ -1,17 +1,17 @@
-%111503049 Mehmet Bozan >> Yapay Zeka 
+%Mehmet Bozan >> Yapay Zeka 
 
-%Kullanılıcak Datalarımda istatistiksel veriler
+%KullanÄ±lÄ±cak DatalarÄ±mda istatistiksel veriler
 
 adet=[7 14 1];
 
 
-%x1=Yol Sathında Gevşek Mazeme
-%x2=Yolda Münferit Çukur
-%x3=Tekerlek İzinde Oturma
-%x4=Kısmi veya Münferit Çökme
-%x5=Düşük Banket
-%x6=Köprü Çökmesi
-%x7=Heyelandan Dolayı Şerit Çökmesi
+%x1=Yol SathÄ±nda GevÅŸek Mazeme
+%x2=Yolda MÃ¼nferit Ã‡ukur
+%x3=Tekerlek Ä°zinde Oturma
+%x4=KÄ±smi veya MÃ¼nferit Ã‡Ã¶kme
+%x5=DÃ¼ÅŸÃ¼k Banket
+%x6=KÃ¶prÃ¼ Ã‡Ã¶kmesi
+%x7=Heyelandan DolayÄ± Åerit Ã‡Ã¶kmesi
 
 girisler=[0 0 0 0 0 0 1;0 0 0 0 0 1 0;0 0 0 0 1 0 0;0 0 0 1 0 0 0;
           0 0 1 0 0 0 0;0 1 0 0 0 0 0;1 0 0 0 0 0 0;0 1 0 1 0 0 0;
@@ -33,10 +33,10 @@ cikislar=[0.012;0.065;0.046;0.054;0.16;0.23;0.42;0.29;0.45;0.66;0.21];
 
 
 hata_payi=1e-2; 
-%datalarıma göre ağ olustur fonksiyonum.
+%datalarÄ±ma gÃ¶re aÄŸ olustur fonksiyonum.
 [esik ,deger]= ag_olustur(adet);
 
-%uygun ağırlık degerimi bulana kadar eğitilcek ağım.fonk. olarak çağrıldı.
+%uygun aÄŸÄ±rlÄ±k degerimi bulana kadar eÄŸitilcek aÄŸÄ±m.fonk. olarak Ã§aÄŸrÄ±ldÄ±.
 [esik ,deger, hata]= ag_egit(girisler,cikislar,esik,deger,adet,hata_payi); 
 
 sonuc=zeros(size(cikislar));
